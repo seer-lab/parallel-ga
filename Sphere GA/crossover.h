a0dfd70bdf8e217aaf95e4509c243b450ece4f57
Taabish Jeshani <taabish.jeshani@uoit.net>
2020-12-22 14:12:51 -0500

    working sphere optimization

--- Sphere GA/crossover.h
+++ Sphere GA/crossover.h
@@ -4,16 +4,16 @@
 
 using std::vector;
 
-void crossover (vector<vector<int>> parents, vector<vector<int>> &offsprings, const int length) {
+void crossover (vector<vector<double>> parents, vector<vector<double>> &offsprings, const int p) {
 
-    int crossoverPoint = rand() % length;
+    int crossoverPoint = rand() % p;
 
     for (vector<int>::size_type i = 0; i != crossoverPoint; i++) {
         offsprings[0][i] = parents[0][i];
         offsprings[1][i] = parents[1][i];
     }
 
-    for (vector<int>::size_type i = crossoverPoint; i != length; i++) {
+    for (vector<int>::size_type i = crossoverPoint; i != p; i++) {
         offsprings[0][i] = parents[1][i];
         offsprings[1][i] = parents[0][i];
     }
