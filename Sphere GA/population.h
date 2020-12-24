48905bb821b4a83cb5a7347acdf88aa2dc514402
Taabish Jeshani <taabish.jeshani@uoit.net>
2020-12-23 23:57:02 -0500

    Updating sphere GA

--- Sphere GA/population.h
+++ Sphere GA/population.h
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <vector>
 
+
+// Generates random values between -5.12 and 5.12
 void initPopulation (std::vector<std::vector<double>> &population, std::vector<float> bounds) {
+
     for (auto i = population.begin(); i != population.end(); i++){
         for (auto j = i->begin(); j != i->end(); j++) {
             *j = bounds[0] + ((double)rand() / RAND_MAX) * (bounds[1] - bounds[0]);
